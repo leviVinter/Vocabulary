aa4d9c5fa4a8c07ea48dc8bd9de1f0f710eadfd7
leviVinter <thomas.takyon@gmail.com>
2016-07-21 19:58:58 +0200

    added in roadmap section

--- script/japanese.js
+++ script/japanese.js
@@ -1,9 +1,14 @@
 function toggleDropDown(e) {
     var x = document.getElementById(e.currentTarget.id + "Block");
     if (activeBlock) {
-        if (x.id != activeBlock) {
-            document.getElementById(activeBlock).className = "hideDropDown";
+        if (x.id != activeBlock && x.parentNode.id != activeBlock) {
+            var a = document.getElementById(activeBlock);
+            a.className = "hideDropDown";
+            if (a.parentNode.className == "showDropDown") {
+                a.parentNode.className = "hideDropDown";
+            }
         }
+
     }
     if (x.className === "hideDropDown") {
         x.className = "showDropDown";
@@ -18,10 +23,16 @@ function outsideClick(e) {
         var w = document.getElementById("addWord");
         var c = document.getElementById("addCategory");
         var r = document.getElementById("roadmaps");
+        var ar = document.getElementById("addRoadmap");
         var t = e.target;
         if (t != a && t.parentNode != a && t.parentNode.parentNode != a && 
-            t != w && t != c && t != r) {
-            document.getElementById(activeBlock).className = "hideDropDown";
+            t.parentNode.parentNode != a &&
+            t != w && t != c && t != r && t != ar) {
+            var active = document.getElementById(activeBlock);
+            active.className = "hideDropDown";
+            if (active.parentNode.className == "showDropDown") {
+                active.parentNode.className = "hideDropDown";
+            }
         }
     }
 }
@@ -29,8 +40,10 @@ var activeBlock = null;
 var word = document.getElementById("addWord");
 var category = document.getElementById("addCategory");
 var roadmaps = document.getElementById("roadmaps")
+var addRoadmap = document.getElementById("addRoadmap");
 
 word.addEventListener("mouseup", toggleDropDown);
 category.addEventListener("mouseup", toggleDropDown);
 roadmaps.addEventListener("mouseup", toggleDropDown);
+addRoadmap.addEventListener("mouseup", toggleDropDown);
 window.addEventListener("mousedown", outsideClick);
