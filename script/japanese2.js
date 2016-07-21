0c18cea64efd07b3f6084ba9d339117b3f71ccec
leviVinter <thomas.takyon@gmail.com>
2016-07-21 03:04:05 +0200

    fixed class and id names

--- script/japanese2.js
+++ script/japanese2.js
@@ -1,20 +1,21 @@
 function showWordForm(e) {
-    var x = document.getElementById("wordForm");
-    if (x.className === "wordForm") {
+    var x = document.getElementById("addWordForm");
+    if (x.className === "addWordForm") {
         x.className = "show";
     } else {
-        x.className = "wordForm";
+        x.className = "addWordForm";
     }  
 }
 
-var h4 = document.getElementById("h4");
+var h4 = document.getElementById("addWordHead");
 
 h4.addEventListener("mouseup", showWordForm);
 
 window.addEventListener("mousedown", function(e) {
-    var x = document.getElementById("wordForm");
-    var y = e.target.parentNode;
-    if (e.target != x && y != x && y.parentNode != x) {
-        document.getElementById("wordForm").className = "wordForm";
+    var x = document.getElementById("addWordForm");
+    var y = document.getElementById("addWordHead");
+    var z = e.target;
+    if (z != x && z.parentNode != x && z.parentNode.parentNode != x && z != y) {
+        document.getElementById("addWordForm").className = "addWordForm";
     }
 });
