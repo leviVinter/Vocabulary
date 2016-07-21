4026392ed41839eea4b44b3f76bbe1c2e0e47f5e
Canjono <michael.hedlund88@gmail.com>
2016-07-21 23:55:27 +0200

    fixed javascript roadmap places

--- script/japanese.js
+++ script/japanese.js
@@ -29,7 +29,6 @@ function outsideClick(e) {
         var ar = document.getElementById("addRoadmap");
         var t = e.target;
         if (t != a && t.parentNode != a && t.parentNode.parentNode != a && 
-            t.parentNode.parentNode != a &&
             t != w && t != c && t != r && t != ar) {
             var active = document.getElementById(activeBlock);
             active.className = "hideDropDown";
@@ -39,14 +38,46 @@ function outsideClick(e) {
         }
     }
 }
+function createInputText(e) {
+    if(document.getElementsByClassName("roadmapPlaces")) {
+        var removeInput = document.getElementsByClassName("roadmapPlaces");
+        var n = removeInput.length;
+        for(var i = 0; i < n; i++) {
+            removeInput[0].parentNode.removeChild(removeInput[0]);
+        }
+    }
+    var x = document.getElementById(e.currentTarget.id);
+    var n = x.options[x.selectedIndex].value;
+    var div = document.createDocumentFragment();
+    var input = null;
+    for(var i = 0; i < n; i++) {
+        input = document.createElement("input");
+        input.setAttribute("type", "text");
+        input.setAttribute("class", "roadmapPlaces")
+        div.appendChild(input);
+    }
+    var br = document.createElement("br");
+    div.appendChild(br);
+    var submit = document.createElement("input");
+    submit.setAttribute("type", "submit");
+    submit.setAttribute("value", "Submit");
+    submit.setAttribute("class", "roadmapPlaces");
+    div.appendChild(submit);
+    
+    document.getElementById("roadmapForm").appendChild(div);
+}
 var activeBlock = null;
 var word = document.getElementById("addWord");
 var category = document.getElementById("addCategory");
 var roadmaps = document.getElementById("roadmaps")
 var addRoadmap = document.getElementById("addRoadmap");
+var selectRoadmap = document.getElementById("selectRoadmap");
 
 word.addEventListener("mouseup", toggleDropDown);
 category.addEventListener("mouseup", toggleDropDown);
 roadmaps.addEventListener("mouseup", toggleDropDown);
 addRoadmap.addEventListener("mouseup", toggleDropDown);
+selectRoadmap.addEventListener("change", createInputText);
 window.addEventListener("mousedown", outsideClick);
+
+
