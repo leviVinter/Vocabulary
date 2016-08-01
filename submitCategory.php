0e201dedda0b5a09af32c8c1ebc1a47a7dd8fe67
Canjono <michael.hedlund88@gmail.com>
2016-08-01 20:05:27 +0200

    added submitCategory.php

---
+++ submitCategory.php
@@ -0,0 +1,19 @@
+<?php
+    if(isset($_POST['newCategory'])) {
+
+    require_once 'login.php';
+    $conn = new mysqli($hn, $un, $pw, $db);
+    if($conn->connect_error) die($conn->connect_error);
+
+    header("Content-Type: application/json");
+    
+    $newCategory = $_POST['newCategory'];
+    $query = "INSERT INTO categories VALUES(NULL,'$newCategory',1)";
+    $result = $conn->query($query);
+    if(!$result) die("Database access failed: " . $conn->error);
+
+    echo json_encode("Category was added");
+
+    $conn->close();
+    }
+?>
